f41e20522a3da40a0c9350504df0cbaeec4f4734
Hritwik <Hritwiksinghal@outlook.in>
2021-10-29 02:45:56 +0530

    get debug flag from environment on start

--- set_debug.py
+++ set_debug.py
@@ -3,6 +3,6 @@ import os
 
 _LOGGER = logging.getLogger(__name__)
 
-if os.path.isfile('test_bit'):
+if os.path.isfile('test_bit') or os.environ.get("DEBUG", 0):
     os.environ["DEBUG"] = "1"
     _LOGGER.debug(f"Setting Debug env var to: {os.environ['DEBUG']}")
