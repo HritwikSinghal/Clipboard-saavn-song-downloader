6586a2809044870e75c55512f757c2b288bb0d07
Hritwik <hritwik.s@media.net>
2023-09-03 03:35:15 +0530

    update git repo if present

--- install_linux.sh
+++ install_linux.sh
@@ -11,19 +11,18 @@ end=$'\e[0m'
 
 # from https://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
 SCRIPTPATH="$(
-  cd "$(dirname "$0")" >/dev/null 2>&1
-  pwd -P
+    cd "$(dirname "$0")" >/dev/null 2>&1
+    pwd -P
 )"
 SCRIPT_PATH=$(dirname $(realpath -s $0))
 
 # Check command availability
 function has_command() {
-  command -v $1 >/dev/null
+    command -v $1 >/dev/null
 }
 
 # if running as root user then set current user as sudo user (only if sudo_user is there)
-if [ "$EUID" -ne 0 ] && [ -n "${SUDO_USER}" ]
-then
+if [ "$EUID" -ne 0 ] && [ -n "${SUDO_USER}" ]; then
     export $USER=$SUDO_USER
     export $HOME=USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
 fi
@@ -50,27 +49,32 @@ echo -e "
 
 printf "\n\n ${grn} Installing git, python3, pipenv ${end} "
 if has_command apt; then
-  apt update -y
-  apt install git python3 python3-pip pipenv -y
+    apt update -y
+    apt install git python3 python3-pip pipenv -y
 elif has_command pacman; then
-  pacman -S git python3 python-pip python-pipenv --noconfirm --needed
+    pacman -S git python3 python-pip python-pipenv --noconfirm --needed
 fi
 
 python3 -m keyring --disable
 
 printf "\n\n ${grn} Cloning Repo ${end} "
-cd $HOME || exit
-rm -rf $HOME/Clipboard-saavn-song-downloader/
-git clone --depth 1 -b master https://github.com/HritwikSinghal/Clipboard-saavn-song-downloader $HOME/Clipboard-saavn-song-downloader/
-cd $HOME/Clipboard-saavn-song-downloader/ || exit
-rm -rf .git/ .gitignore
-chown -R $USER:$USER ./* .
+
+if [ -d $HOME/Clipboard-saavn-song-downloader/ ]; then
+    cd $HOME/Clipboard-saavn-song-downloader/ || exit
+    git add . && git stash
+    git reset HEAD~20 --hard && git pull -f
+    chown -R $USER:$USER ./* .
+else
+    git clone --depth 1 -b master https://github.com/HritwikSinghal/Clipboard-saavn-song-downloader $HOME/Clipboard-saavn-song-downloader/
+    cd $HOME/Clipboard-saavn-song-downloader/ || exit
+    chown -R $USER:$USER ./* .
+fi
 
 printf "\n\n ${grn} Installing xclip, gpaste ${end} "
 if has_command apt; then
-  apt install xclip gpaste -y
+    apt install xclip gpaste -y
 elif has_command pacman; then
-  pacman -S xclip gpaste --noconfirm --needed
+    pacman -S xclip gpaste --noconfirm --needed
 fi
 
 #printf "\n\n ${grn} Installing Xsel, Xclip, wl-clipboard, gpaste ${end} "
@@ -84,12 +88,12 @@ printf "\n\n ${grn} Installing pyenv ${end} "
 if ! has_command pyenv; then
     if has_command apt; then
         curl https://pyenv.run | bash
-        echo 'export PYENV_ROOT="$HOME/.pyenv"' >> $HOME/.bashrc
-        echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> $HOME/.bashrc
-        echo 'eval "$(pyenv init -)"' >> $HOME/.bashrc
-        echo 'eval "$(pyenv virtualenv-init -)"' >> $HOME/.bashrc
+        echo 'export PYENV_ROOT="$HOME/.pyenv"' >>$HOME/.bashrc
+        echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >>$HOME/.bashrc
+        echo 'eval "$(pyenv init -)"' >>$HOME/.bashrc
+        echo 'eval "$(pyenv virtualenv-init -)"' >>$HOME/.bashrc
     elif has_command pacman; then
-      pacman -S pyenv --noconfirm --needed
+        pacman -S pyenv --noconfirm --needed
     fi
 fi
 
@@ -97,8 +101,8 @@ printf "\n ${grn} ------------------------------------------------- ${end} "
 cd $HOME/Clipboard-saavn-song-downloader/ || exit
 chmod +x ./clipboard-saavn-song-downloader.py || exit
 echo "cd $HOME/Clipboard-saavn-song-downloader/" > /usr/local/bin/saavn-downloader
-echo "pipenv install" >> /usr/local/bin/saavn-downloader
-echo "pipenv run ./clipboard-saavn-song-downloader.py" >> /usr/local/bin/saavn-downloader
+echo "pipenv install" >>/usr/local/bin/saavn-downloader
+echo "pipenv run ./clipboard-saavn-song-downloader.py" >>/usr/local/bin/saavn-downloader
 chmod +x /usr/local/bin/saavn-downloader
 #ln -sf $HOME/Clipboard-saavn-song-downloader/clipboard-saavn-song-downloader.py /usr/local/bin/saavn-downloader
 
